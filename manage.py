9e7542cc4db0dc2352aaf69e40ea5e56f0ce19f4
retorres <roberthtorres40@yahoo.es>
2021-06-25 10:47:16 -0500

    Agregado un comentario

--- manage.py
+++ manage.py
@@ -5,6 +5,7 @@ import sys
 
 
 def main():
+    #!! Comentario
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'websemantica.settings')
     try:
         from django.core.management import execute_from_command_line
